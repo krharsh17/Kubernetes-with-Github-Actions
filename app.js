669d5e8ff011fb1feeb5051fa0a4b0c69308d036
Rose Chege <chegerose84@gmail.com>
2023-04-03 14:43:56 +0300

    changes

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const rookout = require('rookout');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+rookout.start({
+    token: '611da17334e54f86fadb1b93971e2f201fb8237d1bfe8106d49e181ab8a591f8',labels: {env: 'dev'}})
+
 app.get('/',(req,res) => {
     res.status(200);
-    res.send("Testing K8S deployments");
+    res.send("Hello World!!");
 });
 
 
